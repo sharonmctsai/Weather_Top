0d2a954e84ecd89de0e384955a34ed15acc54635
Sharon Tsai <sharonmctsai@gmail.com>
2023-05-20 19:33:14 +0100

    prepare for play application for deployment

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -5,6 +5,10 @@ import models.Reading;
 import play.Logger;
 import play.mvc.Controller;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 public class StationCtrl extends Controller
 {
   public static void index(Long id)
@@ -14,8 +18,9 @@ public class StationCtrl extends Controller
     render("station.html", station);
   }
 
-  public static void addReading(Long id, String date, int code, double temperature, double windSpeed, int windDirection, int pressure)
+  public static void addReading(Long id, int code, double temperature, double windSpeed, int windDirection, int pressure)
   {
+    String date = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss").format(LocalDateTime.now(ZoneOffset.UTC));
     Reading reading = new Reading (date,code,temperature,windSpeed,windDirection, pressure);
     Station station = Station.findById(id);
     station.readings.add(reading);
